8697e9bc0e184ad784450d6061f920de9d7fcfaf
innocentAC <innocentelvis46@gmail.com>
2022-10-16 13:21:35 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,11 +2,12 @@
 /**
  *main-program entry point.
  *Return:0 if no error, non zero if error.
- **/
+ */
 int main(void)
 {
 int i;
 for ('i = '0'; i <= '9'; i++)
 putchar(i);
+putchar('\n');
 return (0);
 }
