034f6f714f5252dddc095524a75e9c3b7209025c
innocentAC <innocentelvis46@gmail.com>
2022-10-28 11:29:38 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#incude "main.h"
+/**
+ *_strcat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *Return: a pointer to the resulting dest string
+ */
+char *_strcat(char *dest, char *src)
+{
+char *val = strcat(dest, src);
+return (val);
+}
